a01a743c9cf7d3c80631c0cb2068870f719536c5
henrikmutt <henrik.mutt@gmail.com>
2024-02-09 23:34:53 +0200

    zero

--- script.js
+++ script.js
@@ -63,20 +63,24 @@ fetch('https://script.google.com/macros/s/AKfycbxWsoLI6Vsu7Y3SHFJIXG5GR2rZTmLePD
     return data.json();
 }).then((objectData) => {
 
-    let tableData='';
+    let tableData = '';
     objectData.data.slice(1).map((value) => {
         tableData += `<tr>
-            <td><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
-          </td>
+            <td></td>
             <td>${value.TÄISNIMI}</td>
             <td></td>
             <td>${value["GOOFY VÕI REGU"]}</td>
             <td>${value["VÕISTLUS NR"]}</td>
-    </tr>
-    <hr/>`;
-
+            </tr>
+            <tr>
+            <div id="addPoints">
+            <p><strong>1.Run</strong></p>
+            <p id="firstRun"></p>
+            <p><strong>2.Run</strong></p>
+            <p id="secondRun"></p>
+            </div>
+            </tr>
+            <hr/>`;
 
     });
     document.getElementById('listBody').innerHTML=tableData;
@@ -85,5 +89,57 @@ fetch('https://script.google.com/macros/s/AKfycbxWsoLI6Vsu7Y3SHFJIXG5GR2rZTmLePD
 
 
 
+// PROOV 
 
+/* fetch('https://script.google.com/macros/s/AKfycbxWsoLI6Vsu7Y3SHFJIXG5GR2rZTmLePDlOG-zAEdUV7tJ36p7tEqq-958DZicdweog/exec').then((data) => {
+    return data.json();
+}).then((objectData) => {
+    const athletesList = objectData.data
 
+    athletesList.slice(1).forEach(athlete => {
+        const athleteName = athlete.TÄISNIMI;
+        const style = athlete["GOOFY VÕI REGU"];
+        const nr = athlete["VÕISTLUS NR"];
+
+        const listItem = document.createElement('tr');
+        listItem.setAttribute('type', 'button');
+        listItem.setAttribute('id', 'athlete');
+
+        const addPoints = document.createElement('tr');
+        addPoints.setAttribute('id', 'addPoints')
+
+        listItem.innerHTML = `
+            <td><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg></td>
+            <td>${athleteName}</td>
+            <td></td>
+            <td></td>
+            <td>${style}</td>
+            <td>${nr}</td>`;
+
+        addPoints.innerHTML = `<div>${athleteName}</div>`
+
+        const listBody = document.getElementById('listBody');
+        listBody.append(listItem);
+        listBody.append(addPoints)
+
+        listItem.addEventListener('click', () => {
+            showAddPoints(athlete);
+
+        addPoints.addEventListener('click', () => {
+            hideAddPoints();
+        })
+        })
+
+        
+function showAddPoints() {
+    addPoints.style.display = 'block';
+}
+
+function hideAddPoints() {
+    addPoints.style.display = 'none';
+}
+
+    });
+}); */

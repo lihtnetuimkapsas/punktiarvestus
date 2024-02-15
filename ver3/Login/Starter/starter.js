c41f78824baaf2778b09469700f99c8b92c9fddf
henrikmutt <henrik.mutt@gmail.com>
2024-02-15 22:37:29 +0200

    admin delete

--- ver3/Login/Starter/starter.js
+++ ver3/Login/Starter/starter.js
@@ -78,3 +78,14 @@ function remaingCount() {
     }
 };
 
+
+/* // SEE LÄHEB KOHTUNIKU LEHELE
+
+// Display the nextAthlete variable in the other page
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nextAthlete = localStorage.getItem('nextAthlete');
+    if (nextAthlete) {
+        console.log(nextAthlete)
+    }
+}); */
